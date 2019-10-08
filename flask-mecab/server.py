271e24d6e476700c0e30250f4f14d217733cbf4a
Sau Yee Chan <y26805@gmail.com>
2019-10-08 13:59:21 +0900

    Remove mecab-ipadic dictionary

--- flask-mecab/server.py
+++ flask-mecab/server.py
@@ -10,17 +10,6 @@ cors = CORS(app, resources={r"/mecab/*": {"origins": "*"}})
 messages = ['Success', 'Faild']
 
 
-@app.route('/mecab/v1/parse-ipadic', methods=['POST'])
-def parse():
-    if not (request.json and 'sentence' in request.json):
-        abort(400)
-
-    sentence = request.json['sentence']
-    results = mecab_parse(sentence)
-
-    return mecab_response(200, messages[0], results, 'ipadic')
-
-
 @app.route('/mecab/v1/parse-neologd', methods=['POST'])
 def parse_neologd():
     if not (request.json and 'sentence' in request.json):
@@ -51,7 +40,8 @@ def mecab_parse(sentence, dic='ipadic'):
     m = MeCab.Tagger('-d ' + dic_dir + dic_name)
 
     # 出力フォーマット（デフォルト）
-    format = ['表層形', '品詞', '品詞細分類1', '品詞細分類2', '品詞細分類3', '活用形', '活用型','原型','読み','発音']
+    format = ['表層形', '品詞', '品詞細分類1', '品詞細分類2',
+              '品詞細分類3', '活用形', '活用型', '原型', '読み', '発音']
 
     return [dict(zip(format, (lambda x: [x[0]]+x[1].split(','))(p.split('\t')))) for p in m.parse(sentence).split('\n')[:-2]]
 
